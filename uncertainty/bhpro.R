c82bf73d11d9165022dde07e6d1368d75a7d819f
Ethan Lawler <lawlerem@gmail.com>
2024-01-17 12:02:57 -0400

    Added Beverton-Holt profile example

---
+++ uncertainty/bhpro.R
@@ -0,0 +1,23 @@
+library(RTMB)
+dat <- read.table("bh.dat", header=TRUE)
+par <- list(loga=0, logb=0, logsigma=0)
+
+nll<-function(par){
+  getAll(par, dat)
+  sigma <- exp(logsigma)
+  pred <- loga+log(ssb)-log(1+exp(logb)*ssb)
+  nll <- -sum(dnorm(logR,pred,sigma,TRUE))
+  a <- exp(loga)
+  ADREPORT(a)
+  return(nll)
+}
+
+obj <- MakeADFun(nll, par, silent=TRUE)
+opt <- nlminb(obj$par, obj$fn, obj$gr)
+
+ls.pro<-TMB:::tmbprofile(obj, "logsigma")
+plot(ls.pro)
+confint(ls.pro)
+
+
+#
